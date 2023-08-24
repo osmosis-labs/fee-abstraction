d166eb4d134670a541547c0a4515676a088222d4
GnaD13 <cpt.meoz@gmail.com>
2023-08-24 15:12:41 +0700

    ict

--- tests/interchaintest/ibc_transfer_test.go
+++ tests/interchaintest/ibc_transfer_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"testing"
 
+	"cosmossdk.io/math"
 	sdktypes "github.com/cosmos/cosmos-sdk/types"
 
 	transfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
@@ -122,7 +123,7 @@ func TestFeeabsGaiaIBCTransfer(t *testing.T) {
 	require.Equal(t, genesisWalletAmount, gaiaOrigBal)
 
 	// Compose an IBC transfer and send from feeabs -> Gaia
-	const transferAmount = int64(1_000)
+	transferAmount := math.NewInt(1_000)
 	transfer := ibc.WalletAmount{
 		Address: gaiaUserAddr,
 		Denom:   feeabs.Config().Denom,
@@ -149,7 +150,7 @@ func TestFeeabsGaiaIBCTransfer(t *testing.T) {
 	// Assert that the funds are no longer present in user acc on feeabs and are in the user acc on Gaia
 	feeabsUpdateBal, err := feeabs.GetBalance(ctx, feeabsUserAddr, feeabs.Config().Denom)
 	require.NoError(t, err)
-	require.Equal(t, feeabsOrigBal-transferAmount, feeabsUpdateBal)
+	require.Equal(t, feeabsOrigBal.Sub(transferAmount), feeabsUpdateBal)
 
 	gaiaUpdateBal, err := gaia.GetBalance(ctx, gaiaUserAddr, feeabsIBCDenom)
 	require.NoError(t, err)
