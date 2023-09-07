7d75638c8c350f8ee337ddfaf6796d0cad56b809
GNaD <cpt.meoz@gmail.com>
2023-09-07 09:46:43 +0700

    delete OsmosisIBCDenom

--- x/feeabs/keeper/config.go
+++ x/feeabs/keeper/config.go
@@ -59,9 +59,15 @@ func (k Keeper) SetHostZoneConfig(ctx sdk.Context, chainConfig types.HostChainFe
 }
 
 func (k Keeper) DeleteHostZoneConfig(ctx sdk.Context, ibcDenom string) error {
+	hostZoneConfig, _ := k.GetHostZoneConfig(ctx, ibcDenom)
 	store := ctx.KVStore(k.storeKey)
+
 	key := types.GetKeyHostZoneConfigByFeeabsIBCDenom(ibcDenom)
 	store.Delete(key)
+
+	key = types.GetKeyHostZoneConfigByOsmosisIBCDenom(hostZoneConfig.OsmosisPoolTokenDenomIn)
+	store.Delete(key)
+
 	return nil
 }
 
