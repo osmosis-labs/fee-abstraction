5fd07f046e8a2b3f77a699e719aa9f1f60a07861
tnv1 <tien@notional.ventures>
2024-01-24 16:54:29 +0700

    Refactor code in app and cmd

--- cmd/feeappd/cmd/root.go
+++ cmd/feeappd/cmd/root.go
@@ -60,7 +60,7 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 
 	rootCmd := &cobra.Command{
 		Use:   "feeappd",
-		Short: "Stargate Cosmos Hub App",
+		Short: "Fee Abstraction App",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
 			initClientCtx, err := client.ReadPersistentCommandFlags(initClientCtx, cmd.Flags())
 			if err != nil {
