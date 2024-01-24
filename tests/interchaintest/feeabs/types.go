4f07e2095555e36be7891badc8f96cf84a34f3c7
tnv1 <tien@notional.ventures>
2024-01-24 13:59:04 +0700

    Add query osmosis arithmetic twap for interchaintest.

--- tests/interchaintest/feeabs/types.go
+++ tests/interchaintest/feeabs/types.go
@@ -1,9 +1,5 @@
 package feeabs
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-)
-
 type HostChainFeeAbsConfigResponse struct {
 	HostChainConfig HostChainFeeAbsConfig `json:"host_chain_config"`
 }
@@ -13,16 +9,4 @@ type HostChainFeeAbsConfig struct {
 	OsmosisPoolTokenDenomIn string `json:"osmosis_pool_token_denom_in"`
 	PoolId                  string `json:"pool_id"`
 	Frozen                  bool   `json:"frozen"`
-}
-
-type AddHostZoneProposalType struct {
-	Title           string                `protobuf:"bytes,1,opt,name=title,proto3" json:"title,omitempty"`
-	Description     string                `protobuf:"bytes,2,opt,name=description,proto3" json:"description,omitempty"`
-	HostChainConfig HostChainFeeAbsConfig `protobuf:"bytes,3,opt,name=host_chain_config,json=hostChainConfig,proto3" json:"host_chain_config,omitempty"`
-	Deposit         string                `json:"deposit"`
-}
-
-type QueryFeeabsModuleBalacesResponse struct {
-	Balances sdk.Coins
-	Address  string
 }
