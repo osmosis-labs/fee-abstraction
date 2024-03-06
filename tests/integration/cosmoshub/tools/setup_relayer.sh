303b4cddfba9a19ea36325504ed386447368f930
tnv1 <tien@notional.ventures>
2024-03-06 16:45:19 +0700

    Update docs

--- tests/integration/cosmoshub/tools/setup_relayer.sh
+++ tests/integration/cosmoshub/tools/setup_relayer.sh
@@ -8,7 +8,3 @@ rly paths add-dir relayers/paths
 rly keys restore gaia relayer "bomb jewel cushion behave orphan lava bulk defy evolve spend match grass dress upgrade blast please business stairs learn syrup kick narrow bleak canoe"
 rly keys restore osmosis relayer "bomb jewel cushion behave orphan lava bulk defy evolve spend match grass dress upgrade blast please business stairs learn syrup kick narrow bleak canoe"
 
-rly tx link transfer -d -t 10s --client-tp 36h
-rly tx link query -d -t 10s --client-tp 36h
-
-rly start
